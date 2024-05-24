e77cbee60105e5b15cbbc6ba99998fd3f3100eb8
Fenil Dave <fenil1104dave@gmail.com>
2024-05-24 19:16:43 +0530

    Added login form.

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,3 +1,28 @@
+import { useForm } from "react-hook-form";
+
+type LoginForm = {
+  username: string;
+  password: string;
+};
+
 export const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm<LoginForm>();
+
+  const onSubmit = (data: LoginForm) => {
+    console.log({ data });
+  };
+  return (
+    <div className="root">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("username", { required: true })} />
+          <input
+            type="password"
+            {...register("password", { required: true })}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
