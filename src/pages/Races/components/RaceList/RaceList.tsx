4e3dc4bd0e41ac89500e1d40418f9be8ccad1028
Fenil Dave <fenil1104dave@gmail.com>
2024-05-27 19:29:08 +0530

    Added getRace socket.

--- src/pages/Races/components/RaceList/RaceList.tsx
+++ src/pages/Races/components/RaceList/RaceList.tsx
@@ -4,30 +4,31 @@ import { getAllRaces } from "../../../../api/race/raceAPI";
 import { Race } from "../../../../api/race/types";
 
 const columns = [
-  { field: "_id", headerName: "ID", width: 200 },
-  { field: "name", headerName: "Name" },
-  { field: "started_at", headerName: "Started At" },
-  {
-    field: "players",
-    headerName: "Player",
-    valueGetter: (_, row: Race) =>
-      row.players
-        .map((player) => `${player.horse.name}(${player.lane_number})`)
-        .join(","),
-  },
+    { field: "_id", headerName: "ID", width: 200 },
+    { field: "name", headerName: "Name" },
+    { field: "started_at", headerName: "Started At" },
+    {
+        field: "players",
+        headerName: "Player",
+        valueGetter: (_, row: Race) =>
+            row.players
+                .map((player) => `${player.horse.name}(${player.lane_number})`)
+                .join(","),
+        width: 200,
+    },
 ];
 
 export const RaceList = () => {
-  const { data = [], isLoading } = useQuery<Race[]>({
-    queryKey: ["races"],
-    queryFn: getAllRaces,
-  });
-  return (
-    <DataGrid
-      rows={data}
-      loading={isLoading}
-      getRowId={(row) => row._id}
-      columns={columns}
-    />
-  );
+    const { data = [], isLoading } = useQuery<Race[]>({
+        queryKey: ["races"],
+        queryFn: getAllRaces,
+    });
+    return (
+        <DataGrid
+            rows={data}
+            loading={isLoading}
+            getRowId={(row) => row._id}
+            columns={columns}
+        />
+    );
 };
